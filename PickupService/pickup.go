225ee430b07ee104974d6cfdc1d1d40c0e9c22ff
r <naurafaizah@upi.edu>
2026-05-21 07:47:25 +0700

    Update pickup.go

--- PickupService/pickup.go
+++ PickupService/pickup.go
@@ -3,7 +3,8 @@ package main
 func ProcessPickup(paymentStatus string, weight int) string {
 
 	if paymentStatus == "paid" && weight > 0 {
-		return "failed"
+		return "scheduled"
 	}
-	return "failed"
+
+	return "pending"
 }
